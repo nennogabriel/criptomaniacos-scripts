b713170efb5cfa1b05f72d17c23c60e64f640a96
Pedro Moreno <nennogabriel@gmail.com>
2022-03-24 13:18:41 -0300

    first code

---
+++ public/membros.js
@@ -0,0 +1,14 @@
+var $_Huggy = {
+  defaultCountry: '+55',
+  uuid: 'eb23b2fe-ab27-4358-b165-66f50a7cdaf3',
+  company: '335060',
+};
+(function (i, s, o, g, r, a, m) {
+  i[r] = { context: { id: '6f572ea0161fbcb0a44168df7c23ea65' } };
+  a = o;
+  o = s.createElement(o);
+  o.async = 1;
+  o.src = g;
+  m = s.getElementsByTagName(a)[0];
+  m.parentNode.insertBefore(o, m);
+})(window, document, 'script', 'https://js.huggy.chat/widget.min.js', 'pwz');
---
+++ public/site.js
@@ -0,0 +1,28 @@
+var hour = new Date().getUTCHours();
+var $_Huggy = {
+  defaultCountry: '+55',
+  uuid: 'b7bc2fce-928a-407b-b55e-228a1a3df0dd',
+  company: '335060',
+  channelGreeting: 'Olá! Gostaria de conversar com o time de atendimento.',
+};
+if (hour >= 11 && hour < 22) {
+  (function (i, s, o, g, r, a, m) {
+    i[r] = { context: { id: '6f572ea0161fbcb0a44168df7c23ea65' } };
+    a = o;
+    o = s.createElement(o);
+    o.async = 1;
+    o.src = g;
+    m = s.getElementsByTagName(a)[0];
+    m.parentNode.insertBefore(o, m);
+  })(window, document, 'script', 'https://js.huggy.chat/widget.min.js', 'pwz');
+} else {
+  (function (i, s, o, g, r, a, m) {
+    i[r] = { context: { id: '6f572ea0161fbcb0a44168df7c23ea65' } };
+    a = o;
+    o = s.createElement(o);
+    o.async = 1;
+    o.src = g;
+    m = s.getElementsByTagName(a)[0];
+    m.parentNode.insertBefore(o, m);
+  })(window, document, 'script', 'https://js.huggy.chat/widget.min.js', 'pwz');
+}
