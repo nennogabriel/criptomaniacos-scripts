049bd5a6217f6c0f0a1cd5de28856c5f3b5fe79b
Pedro Moreno <nennogabriel@gmail.com>
2022-03-24 20:08:58 -0300

    feat: huggy to whastapp

--- public/membros.js
+++ public/membros.js
@@ -3,12 +3,32 @@ var $_Huggy = {
   uuid: 'eb23b2fe-ab27-4358-b165-66f50a7cdaf3',
   company: '335060',
 };
-(function (i, s, o, g, r, a, m) {
-  i[r] = { context: { id: '6f572ea0161fbcb0a44168df7c23ea65' } };
-  a = o;
-  o = s.createElement(o);
-  o.async = 1;
-  o.src = g;
-  m = s.getElementsByTagName(a)[0];
-  m.parentNode.insertBefore(o, m);
-})(window, document, 'script', 'https://js.huggy.chat/widget.min.js', 'pwz');
+
+fetch('https://s.cmania.co/api/fauna/huggy')
+  .then((r) => r.json())
+  .then((j) => {
+    const { huggy } = j;
+    if (huggy) {
+      (function (i, s, o, g, r, a, m) {
+        i[r] = { context: { id: '6f572ea0161fbcb0a44168df7c23ea65' } };
+        a = o;
+        o = s.createElement(o);
+        o.async = 1;
+        o.src = g;
+        m = s.getElementsByTagName(a)[0];
+        m.parentNode.insertBefore(o, m);
+      })(
+        window,
+        document,
+        'script',
+        'https://js.huggy.chat/widget.min.js',
+        'pwz'
+      );
+    } else {
+      !(function (b, t, c) {
+        ((c = t.createElement(b)).async = 1),
+          (c.src = 'http://s.cmania.co/tools/whatsapp.js'),
+          (b = t.getElementsByTagName(b)[0]).parentNode.insertBefore(c, b);
+      })('script', document);
+    }
+  });
--- public/site.js
+++ public/site.js
@@ -1,31 +1,35 @@
-var hour = new Date().getUTCHours();
-fetch('https://s.cmania.co/api/fauna/huggy')
-  .then((r) => console.log(JSON.stringify(r)))
-  .catch((e) => console.log(e));
 var $_Huggy = {
   defaultCountry: '+55',
   uuid: 'b7bc2fce-928a-407b-b55e-228a1a3df0dd',
   company: '335060',
   channelGreeting: 'Olá! Gostaria de conversar com o time de atendimento.',
 };
-if (hour >= 11 && hour < 22) {
-  (function (i, s, o, g, r, a, m) {
-    i[r] = { context: { id: '6f572ea0161fbcb0a44168df7c23ea65' } };
-    a = o;
-    o = s.createElement(o);
-    o.async = 1;
-    o.src = g;
-    m = s.getElementsByTagName(a)[0];
-    m.parentNode.insertBefore(o, m);
-  })(window, document, 'script', 'https://js.huggy.chat/widget.min.js', 'pwz');
-} else {
-  (function (i, s, o, g, r, a, m) {
-    i[r] = { context: { id: '6f572ea0161fbcb0a44168df7c23ea65' } };
-    a = o;
-    o = s.createElement(o);
-    o.async = 1;
-    o.src = g;
-    m = s.getElementsByTagName(a)[0];
-    m.parentNode.insertBefore(o, m);
-  })(window, document, 'script', 'https://js.huggy.chat/widget.min.js', 'pwz');
-}
+
+fetch('https://s.cmania.co/api/fauna/huggy')
+  .then((r) => r.json())
+  .then((j) => {
+    const { huggy } = j;
+    if (huggy) {
+      (function (i, s, o, g, r, a, m) {
+        i[r] = { context: { id: '6f572ea0161fbcb0a44168df7c23ea65' } };
+        a = o;
+        o = s.createElement(o);
+        o.async = 1;
+        o.src = g;
+        m = s.getElementsByTagName(a)[0];
+        m.parentNode.insertBefore(o, m);
+      })(
+        window,
+        document,
+        'script',
+        'https://js.huggy.chat/widget.min.js',
+        'pwz'
+      );
+    } else {
+      !(function (b, t, c) {
+        ((c = t.createElement(b)).async = 1),
+          (c.src = 'http://s.cmania.co/tools/whatsapp.js'),
+          (b = t.getElementsByTagName(b)[0]).parentNode.insertBefore(c, b);
+      })('script', document);
+    }
+  });
