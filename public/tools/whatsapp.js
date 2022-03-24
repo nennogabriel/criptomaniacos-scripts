2ca32ea504619bcffe914ea0429fad6f4cb16ffe
Pedro Moreno <nennogabriel@gmail.com>
2022-03-24 20:33:17 -0300

    fix: whatsapp target blank

--- public/tools/whatsapp.js
+++ public/tools/whatsapp.js
@@ -6,6 +6,7 @@ var $_CM = {
 !(function () {
   const e = document.createElement('a');
   (e.href = `https://wa.me/${$_CM.phone}?text=${$_CM.message}`),
+    (e.target = '_blank'),
     (e.style.display = 'block'),
     (e.style.position = 'fixed'),
     (e.style.bottom = '20px'),
@@ -16,7 +17,7 @@ var $_CM = {
     (e.style.color = '#fff'),
     (e.style.padding = '8px 16px'),
     (e.style.borderRadius = '8px'),
-    e.appendChild(document.createTextNode('Whatsapp'));
+    e.appendChild(document.createTextNode('WhatsApp'));
   var t = 'div';
   (t = document.getElementsByTagName(t)[0]).parentNode.insertBefore(e, t);
 })();
