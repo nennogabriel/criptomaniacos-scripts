143a43d2f7f69ed63fa321613cf614c5a4e9311f
Pedro Moreno <nennogabriel@gmail.com>
2022-03-26 10:46:37 -0300

    fix: whatsapp icon

--- public/tools/whatsapp.js
+++ public/tools/whatsapp.js
@@ -3,21 +3,21 @@ var $_CM = {
   message: 'Olá! Gostaria de conversar com o time de atendimento.',
 };
 
-!(function () {
-  const e = document.createElement('a');
-  (e.href = `https://wa.me/${$_CM.phone}?text=${$_CM.message}`),
-    (e.target = '_blank'),
-    (e.style.display = 'block'),
-    (e.style.position = 'fixed'),
-    (e.style.bottom = '20px'),
-    (e.style.right = '20px'),
-    (e.style.zIndex = '9'),
-    (e.style.backgroundColor = '#25D366'),
-    (e.style.textDecoration = 'none'),
-    (e.style.color = '#fff'),
-    (e.style.padding = '8px 16px'),
-    (e.style.borderRadius = '8px'),
-    e.appendChild(document.createTextNode('WhatsApp'));
-  var t = 'div';
-  (t = document.getElementsByTagName(t)[0]).parentNode.insertBefore(e, t);
-})();
+!(function (d) {
+  const e = d.createElement('img');
+  (e.src = '/assets/whatsapp-logo.svg'),
+    (e.style.width = '80px'),
+    (e.style.height = '80px');
+  const t = d.createElement('a');
+  (t.href = `https://wa.me/${$_CM.phone}?text=${$_CM.message}`),
+    (t.target = '_blank'),
+    (t.style.display = 'block'),
+    (t.style.position = 'fixed'),
+    (t.style.bottom = '20px'),
+    (t.style.right = '20px'),
+    (t.style.zIndex = '9'),
+    (t.style.backgroundColor = 'tansparent'),
+    t.appendChild(e);
+  var s = 'div';
+  (s = document.getElementsByTagName(s)[0]).parentNode.insertBefore(t, s);
+})(document);
