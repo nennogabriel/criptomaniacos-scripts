b31dc1f1ce4174f693209e37786430516284d9d9
Pedro Moreno <nennogabriel@gmail.com>
2022-08-12 18:33:15 -0300

    test: guru webhook

--- src/pages/api/guru/webhook/[id].ts
+++ src/pages/api/guru/webhook/[id].ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('req', JSON.stringify(req, null, 2));
-  console.log('res', JSON.stringify(res, null, 2));
+  console.log('req: ', req);
+  console.log('res', res);
   // method not allowed
   return res.status(405).json({ error: 'Method not allowed' });
 }
